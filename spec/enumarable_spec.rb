4f18efec34a1f88afdff4559c02653c86d7ee417
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-01-20 12:01:27 -0400

    add cases for my_each method

---
+++ spec/enumarable_spec.rb
@@ -0,0 +1,21 @@
+# spec/game_logic_test_spec.rbc
+
+require '../lib/enumerable_method'
+
+describe Enumerable do
+  let(:my_range) { Range.new(1, 10) }
+  describe '#my_each' do
+    it 'would return Enumarator if there\'s no block' do
+      expect([1, 2, 3, 4, 5].my_each).to be_an(Enumerator)
+    end
+
+    it 'would return the operation in the block when received a block' do
+      expect([1, 2, 3, 4, 5].my_each { |item| item > 2 }).to eql([1, 2, 3, 4, 5])
+      # since each don't mutate the original array
+    end
+
+    it 'would loop throud in array in execute the block statements on each item' do
+      expect(my_range.my_each { |item| p item * 2 }).to eql(my_range.each { |item| p item * 2 })
+    end 
+  end
+end
