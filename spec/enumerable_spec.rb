7fd329defb80ea2f3f1897eb7226537074cbd780
Ismael Antonio <ismael@pop-os.localdomain>
2021-01-26 19:53:35 +0300

    add test for myeach_with_index

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -12,4 +12,29 @@ describe '#Enumerable' do
       expect((1..4).my_each).to be_a Enumerator
     end
   end
+  describe '#Each With Index' do
+    hash = {}
+    test1 = %w[cat dog wombat].my_each_with_index do |item, index|
+      hash[item] = index
+    end
+    it 'should return an array' do
+      expect(test1).to eql %w[cat dog wombat]
+    end
+    it 'should return an enumerable' do
+      expect((1..4).my_each_with_index).to be_a Enumerator
+    end
+  end
+  describe '#Select' do
+    select1 = [1, 2, 3, 4, 5].my_select(&:even?)
+    select2 = (1..5).my_select(&:even?)
+    it 'should return the filtered array' do
+      expect(select1).to eql [2, 4]
+    end
+    it 'should return the filtred array' do
+      expect(select2).to eql [2, 4]
+    end
+    it 'should return an enumerable' do
+      expect((1..4).my_select).to be_a Enumerator
+    end
+  end
 end
