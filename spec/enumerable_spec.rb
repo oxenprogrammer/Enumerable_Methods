984776ed703d826e5864bf398c98cd9028602a46
oxenprogrammer <emmy1000okello@gmail.com>
2021-01-26 19:57:32 +0300

    test(enumerable): add my_map test cases

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -12,6 +12,7 @@ describe '#Enumerable' do
       expect((1..4).my_each).to be_a Enumerator
     end
   end
+
   describe '#Each With Index' do
     hash = {}
     test1 = %w[cat dog wombat].my_each_with_index do |item, index|
@@ -24,6 +25,7 @@ describe '#Enumerable' do
       expect((1..4).my_each_with_index).to be_a Enumerator
     end
   end
+
   describe '#Select' do
     select1 = [1, 2, 3, 4, 5].my_select(&:even?)
     select2 = (1..5).my_select(&:even?)
@@ -37,6 +39,7 @@ describe '#Enumerable' do
       expect((1..4).my_select).to be_a Enumerator
     end
   end
+
   describe '#Count' do
     my_array = [1, 2, 4, 2]
 
@@ -51,4 +54,28 @@ describe '#Enumerable' do
     end
   end
 
+  describe '#Map' do
+    elements = [1, 2, 3, 4]
+    my_proc = proc { |i| i * i }
+    second_proc = nil
+
+    it 'should return an array doubled given an array' do
+      expect(elements.my_map { |i| i * i }).to eql [1, 4, 9, 16]
+    end
+    it 'should return cats each to array length' do
+      expect(elements.my_map { 'cat' }).to eql %w[cat cat cat cat]
+    end
+    it 'should return an array doubled a range' do
+      expect((1..4).my_map { |i| i * i }).to eql [1, 4, 9, 16]
+    end
+    it 'should return cats each to the range length' do
+      expect((1..4).my_map { 'cat' }).to eql %w[cat cat cat cat]
+    end
+    it 'should return an array given a proc' do
+      expect(elements.my_map(my_proc)).to eql [1, 4, 9, 16]
+    end
+    it 'should return an enumerable' do
+      expect(elements.my_map(second_proc)).to be_a Enumerator
+    end
+  end
 end
